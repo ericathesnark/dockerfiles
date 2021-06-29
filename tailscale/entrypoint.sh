98bf2a15415785390606685421fe3702e74451d2
Erica Garcia <erica@ericas.site>
2021-06-29 12:24:49 -0500

    Update entrypoint.sh

--- tailscale/entrypoint.sh
+++ tailscale/entrypoint.sh
@@ -3,5 +3,5 @@ if [ ! -d /dev/net ]; then mkdir /dev/net; fi
 if [ ! -e /dev/net/tun ]; then  mknod /dev/net/tun c 10 200; fi
 
 # Wait 5s for the daemon to start and then run tailscale up to configure
-/bin/sh -c "sleep 5; /usr/local/bin/tailscale up --authkey=${TAILSCALE_AUTH} -advertise-tags=${TAILSCALE_TAGS}" &
+/bin/sh -c "sleep 5; /usr/local/bin/tailscale up --authkey=${TAILSCALE_AUTH} -advertise-tags=${TAILSCALE_TAGS} ${TAILSCALE_ARGS}" &
 exec /usr/local/bin/tailscaled --state=/tailscale/tailscaled.state
